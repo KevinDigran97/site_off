144896c07c3b17b660b6f99cbb10fa72073e16ac
kevin_digran <kevindigran@gmail.com>
2022-12-28 13:41:01 -0500

    c_hello

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log('Kevin');
+  return (
+    
+    <div className="App">
+       <h1 className="text-5xl font-bold">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
